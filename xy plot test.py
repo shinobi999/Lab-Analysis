c5ecced962460a48edcb90d404042e49c2af4551
shinobi999 <jhoogerhyde01@manhattan.edu>
2019-04-29 13:09:58 -0400

    1st use of dicts to store range of nested values, not fully functional

--- xy plot test.py
+++ xy plot test.py
@@ -3,15 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 '''
-ToDo list: 
+TODO list: 
     develop calculation methods
     develop main loop that breaks CSV file into 28500 event lengths
 
 '''
 
-
 #array for on/off neuromorphic camera values
 on=[]
+#array for on[] arrays
+metaOn={}
 #array for timestamp value of camera samples
 timestamp=[]
 
@@ -33,31 +34,53 @@ class dataChunk:
         return self.covariance
 
     def calcDuration():
-
+        return duration
     def calcDensity():
-
+        return density
     def calcCovariance(): 
+        return covariance
+
+
 
 with open('C:/Users/Admin/Desktop/WaveletAnalysis/wheelTest.csv') as csvfile:
      spamreader = csv.reader(csvfile, delimiter=',')
+     myCount=0
+     loopCount=0
+     innerOn={}
      for row in spamreader:
-        on.append(row[0])
-        timestamp.append(row[1])
+        if myCount==28500:
+            metaOn[loopCount]=[innerOn]
+            myCount=0
+            loopCount+=1 
+            
+        #on.append(row[0])
+        #timestamp.append(row[1])
+        try:
+            innerOn[myCount]=[int(row[0]), int(row[1])]
+        except:
+            continue
+        
+        myCount+=1
+        '''
         try:
             on[-1]=int(on[-1])
             timestamp[-1]=int(timestamp[-1])
+            metaOn[-1]=[int(row[0]), int(row[1])]
         except:
             continue
+        '''
 
 # grab header values from csv
-eventTitle=on.pop(0)
-timeStampTitle=timestamp.pop(0)
+#eventTitle=on.pop(0)
+#timeStampTitle=timestamp.pop(0)
 
 # remove stray startup value
 #del on[0:2]
 #del timestamp[0:2]
 
 # normalize time stamps
+'''
+TODO test loop functionality with dictionary metaOn
 firstTime=timestamp[0]
 for x in range(len(timestamp)):
     timestamp[x]=timestamp[x]-firstTime
@@ -66,4 +89,8 @@ timestampslice=timestamp[0:60]
 onslice=on[0:60]
 plt.scatter(timestampslice, onslice)
 plt.show()
-
+'''
+for x in metaOn:
+    print (x)
+    for y in metaOn[x]:
+        print(y,':', metaOn[x][y])
